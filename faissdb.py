34ab444b623227225d604a0b2931cb61078222c2
gokulan006 <gokulan.m.min24@itbhu.ac.in>
2026-06-25 09:09:36 +0530

    Update faissdb.py

--- faissdb.py
+++ faissdb.py
@@ -1,17 +1,50 @@
 import os
-from dotenv import load_dotenv
-from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
-load_dotenv()
 import pickle
+from langchain_community.vectorstores import FAISS
+from tqdm import tqdm
+from langchain_openai import OpenAIEmbeddings
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-os.environ['HF_TOKEN']=os.getenv('HF_TOKEN')
+ 
+os.environ['OPENAI_API_KEY']=os.getenv('OPENAI_API_KEY')
+
+def create_embedding_fnc(model_name='text-embedding-3-small'):
+    embeddings=OpenAIEmbeddings(
+        model=model_name
+    )
+    return embeddings
 
 with open("Mining_Documents.pkl", "rb") as f:
     documents = pickle.load(f)
  
-embeddings=HuggingFaceEmbeddings(model='sentence-transformers/all-MiniLM-L6-v2')
+embeddings=create_embedding_fnc()
 
-faiss=FAISS.from_documents(documents=documents,embedding=embeddings)
+texts = [d.page_content for d in documents]
+metadatas = [d.metadata for d in documents]
 
-faiss.save_local("new_faiss_index")
+ 
+ 
+all_text_embeddings = []
+all_metas = []
+
+batch_size = 100   
+print("Total number of Text",len(texts))
+for i in range(0, len(texts), batch_size):
+    print("Current Batch",i)
+    batch_texts = texts[i:i+batch_size]
+    batch_metas = metadatas[i:i+batch_size]
+    batch_vectors = embeddings.embed_documents(batch_texts)
+
+     
+    all_text_embeddings.extend(zip(batch_texts, batch_vectors))
+    all_metas.extend(batch_metas)
+
+faiss_db = FAISS.from_embeddings(
+    text_embeddings=all_text_embeddings,
+    embedding=embeddings,
+    metadatas=all_metas
+)
+
+faiss_db.save_local("new_faiss_index")
